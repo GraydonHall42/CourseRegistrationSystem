00a9b62afa640f6637adbc9369e4bee07697fcd9
GraydonHall42 <gwhall@ualberta.ca>
2021-09-09 10:42:59 -0600

    initial commit

---
+++ src/registrationSystem/Course.java
@@ -0,0 +1,32 @@
+package registrationSystem;
+
+public class Course {
+    private String courseName;
+    private String courseNumber;
+
+    public Course(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return this.courseName + " " + this.courseNumber;
+    }
+}
---
+++ src/registrationSystem/FrontEnd.java
@@ -0,0 +1,8 @@
+package registrationSystem;
+
+public class FrontEnd {
+    public static void main(String[] args) {
+        Course myCourse = new Course("ENSF", "607");
+        System.out.println("MyCourse: " + myCourse);
+    }
+}
